6a2be2a5b1bf6859dbefa4a8b9dcce837f230015
Eugene Lim <eugenelimjq1217@gmail.com>
2024-09-13 15:50:49 +0800

    Temporarily removed loading screen

--- app/components/MainHero.tsx
+++ app/components/MainHero.tsx
@@ -6,7 +6,7 @@ import 'animate.css';
 
 export default function MainHero() {
   return (
-      <div className={"w-[100%] h-auto grid place-items-center relative animate__animated animate__fadeInDown animate__delay-3s"}>
+      <div className={"w-[100%] h-auto grid place-items-center relative animate__animated animate__fadeInDown animate__delay-1s"}>
         <Image src={GearRotor} alt={"mainHero"} className={"w-[100%] h-auto max-h-[60dvh] object-cover"}></Image>
         <div className={"w-full h-full absolute top-0 left-0 bg-gray-400 opacity-70 z-10 grid place-items-center"}>
           <h1 className={"text-6xl"}>We add <b>VALUE</b> to the flow</h1>
--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
   return (
       <main>
             <NextUIProvider>
-              <LoadingScreen></LoadingScreen>
+              {/*<LoadingScreen></LoadingScreen>*/}
               <div className={"w-[100dvw] h-auto grid place-items-center bg-gray-200 px-20"}>
                 <MainHero></MainHero>
-                <div className={"grid place-items-center text-black w-[80%] px-10 mt-20 mb-10 animate__animated animate__fadeInUp animate__delay-4s"}>
+                <div className={"grid place-items-center text-black w-[80%] px-10 mt-20 mb-10 animate__animated animate__fadeInUp"}>
                   <h1 className={"text-lg font-semibold"}>What we believe in.</h1>
                   <p className={"mt-5 text-3xl font-light text-center"}>Adding value to your business and growing it alongside us.</p>
                   <p className={"mt-5 mb-20 text-3xl font-light text-center"}>You are our priority.</p>
